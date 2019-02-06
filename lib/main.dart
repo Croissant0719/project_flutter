65af5b143b73774f318e6067137fba1dd6ec97a0
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-02-07 00:32:43 +0900

    Create an infinite scrolling ListView

--- lib/main.dart
+++ lib/main.dart
@@ -6,30 +6,55 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: AppBar(
-          title: Text('Welcome to Flutter'),
-        ),
-        body: Center(
-          child: new RandomWords(),
-        ),
-      ),
+    return new MaterialApp(
+      title: 'Startup Name Generator',
+      home: new RandomWords(),
     );
-  }
-}
-
-class RandomWordsState extends State<RandomWords> {
-  @override
-  Widget build(BuildContext context) {
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
   }
 }
 
 class RandomWords extends StatefulWidget {
   @override
-  RandomWordsState createState() => new RandomWordsState(); 
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestinos(),
+    );
+  }
+
+  Widget _buildSuggestinos() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return new Divider();
+        }
+
+        final int index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
 }
