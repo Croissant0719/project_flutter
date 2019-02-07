4d751d807a83493b1fb0e90c4765b7a794f484ec
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-02-07 22:59:28 +0900

    Update login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final formKey = new GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+
+  bool validateAndSave() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  void validateAndSubmit() {
+    if(validateAndSave()) {
+      
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -8,9 +35,32 @@ class LoginPage extends StatelessWidget {
       appBar: new AppBar(
         title: new Text('Flutter login demo'),
       ),
-        body: new Container(
-          child: new Text('Hello World'),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Form(
+          key: formKey,
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              new TextFormField(
+                decoration: new InputDecoration(labelText: 'Email'),
+                validator: (value) => value.isEmpty ? 'Email can\'t be empry' : null,
+                onSaved: (value) => _email = value,
+              ),
+              new TextFormField(
+                decoration: new InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                validator: (value) => value.isEmpty ? 'Email can\'t be empry' : null,
+                onSaved: (value) => _password = value,
+              ),
+              new RaisedButton(
+                child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+                onPressed: validateAndSubmit,
+              ),
+            ],
+          ),
         ),
+      ),
     );
   }
 }
