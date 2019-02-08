74a6c418b1dc195b7de85ef6c3ebd02bffc182df
Koji Igarashi <koji.igarashi.0719@gmail.com>
2019-02-08 23:46:31 +0900

    Update Login page, add submit and register function

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -6,12 +6,18 @@ class LoginPage extends StatefulWidget {
   State<StatefulWidget> createState() => new _LoginPageState();
 }
 
+enum FormType {
+  login,
+  register
+}
+
 class _LoginPageState extends State<LoginPage> {
 
   final formKey = new GlobalKey<FormState>();
 
   String _email;
   String _password;
+  FormType _formType = FormType.login;
 
   bool validateAndSave() {
     final form = formKey.currentState;
@@ -23,10 +29,34 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
-  void validateAndSubmit() {
+  void validateAndSubmit() async {
     if(validateAndSave()) {
-      
+      try {
+        if (_formType == FormType.login) {
+          FirebaseUser user = await FirebaseAuth.instance.signInWithEmailAndPassword(email: _email, password: _password);
+          print('Sign in: ${user.uid}');
+        } else {
+          FirebaseUser user = await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password);
+          print('Registerd user: ${user.uid}');
+        }
+      }
+      catch (e) {
+        print('Error: $e');
+      }
     }
+  }
+
+  void moveToRegister() {
+    setState(() {
+      formKey.currentState.reset();
+      _formType = FormType.register;    
+    });
+  }
+
+  void moveToLogin() {
+    setState(() {
+      _formType = FormType.login;
+    });
   }
 
   @override
@@ -41,26 +71,53 @@ class _LoginPageState extends State<LoginPage> {
           key: formKey,
           child: new Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              new TextFormField(
-                decoration: new InputDecoration(labelText: 'Email'),
-                validator: (value) => value.isEmpty ? 'Email can\'t be empry' : null,
-                onSaved: (value) => _email = value,
-              ),
-              new TextFormField(
-                decoration: new InputDecoration(labelText: 'Password'),
-                obscureText: true,
-                validator: (value) => value.isEmpty ? 'Email can\'t be empry' : null,
-                onSaved: (value) => _password = value,
-              ),
-              new RaisedButton(
-                child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
-                onPressed: validateAndSubmit,
-              ),
-            ],
+            children: buildInputs() + buildSubmitButtons(),
           ),
         ),
       ),
     );
+  }
+
+  List<Widget> buildInputs() {
+    return [
+      new TextFormField(
+        decoration: new InputDecoration(labelText: 'Email'),
+        validator: (value) => value.isEmpty ? 'Email can\'t be empry' : null,
+        onSaved: (value) => _email = value,
+      ),
+      new TextFormField(
+        decoration: new InputDecoration(labelText: 'Password'),
+        obscureText: true,
+        validator: (value) => value.isEmpty ? 'Email can\'t be empry' : null,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> buildSubmitButtons() {
+    if (_formType == FormType.login) {
+      return [
+        new RaisedButton(
+          child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+          onPressed: validateAndSubmit,
+        ),
+        new FlatButton(
+          child: new Text('Create an account', style: new TextStyle(fontSize: 20.0)),
+          onPressed: moveToRegister,
+        )
+      ];
+    } else {
+      return [
+        new RaisedButton(
+          child: new Text('Create an account', style: new TextStyle(fontSize: 20.0)),
+          onPressed: validateAndSubmit,
+        ),
+        new FlatButton(
+          child: new Text('Hava an account? Login', style: new TextStyle(fontSize: 20.0)),
+          onPressed: moveToLogin,
+        ),
+      ];
+    }
+
   }
 }
